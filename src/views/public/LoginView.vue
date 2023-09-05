65a8041a81e17c54c23a209bb1715d84ba606db2
jvJUCA <jucafranzoni1@gmail.com>
2023-09-05 20:27:51 -0300

    feat: card added, and images for login page

--- src/views/public/LoginView.vue
+++ src/views/public/LoginView.vue
@@ -1,18 +1,41 @@
-<template>
-  <AppBar/>
-  <div class="mb-10 mt-10"></div>
-  <FooterLayout/>
+<template app>
+  <AppBar />
+  <div>
+    <v-card max-width="300px" max-height="1000px" class="mx-auto" color="rgba(113, 29, 216, 0.536)">
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        height="200px"
+        cover
+        class="ma-16"
+      >
+      </v-img>
+      <v-card-title> Entre com seu email de aluno! </v-card-title>
+      <v-btn
+      color="blue-lighten-1"
+      rounded="6"
+      block
+      >Entrar
+      </v-btn>
+    </v-card>
+  </div>
+  <FooterLayout />
 </template>
 
 <script>
 import AppBar from "@/components/AppBar.vue";
-import FooterLayout from "@/components/Footer.vue"
+import FooterLayout from "@/components/Footer.vue";
 export default {
   name: "LoginView",
   components: {
     AppBar,
-    FooterLayout
+    FooterLayout,
   },
   // Outras configurações da visualização
 };
 </script>
+
+<style>
+#app{
+  background-color: rgb(16, 32, 61);
+}
+</style>
