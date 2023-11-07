37f9d7f750d55f5168a7b7a2d5c3cd5cd9669450
jvJUCA <jucafranzoni1@gmail.com>
2023-11-07 18:57:35 -0300

    apaguei a logica?

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,56 +1,60 @@
 // imports
 
-import { auth } from '@/firebase'
+import { auth } from "@/firebase";
+import db from "@/firebase/init";
 
 import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-} from 'firebase/auth'
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 export default class AuthController {
-    //Register new users
+  //Register new users
 
-    async authSignUp(email, password) {
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user
-                return user
-            })
-            .catch(console.error('Error in SignUp'))
-    }
+  async authSignUp(email, password) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        return db.collection("users").doc(userCredential.user.uid).set({
+          email: email,
+          accessLevel: 1,
+          myReports: {},
+          mySuggests: {},
+        });
+      })
+      .catch(console.error("Error in SignUp"));
+  }
 
-    //SignIn
+  //SignIn
 
-    async authSignIn(email, password) {
-        try {
-            return signInWithEmailAndPassword(auth, email, password).then(
-                (userCredential) => {
-                    const user = userCredential.user
-                    return user
-                },
-            )
-        } catch (e) {
-            console.error(e)
+  async authSignIn(email, password) {
+    try {
+      return signInWithEmailAndPassword(auth, email, password).then(
+        (userCredential) => {
+          const user = userCredential.user;
+          return user;
         }
+      );
+    } catch (e) {
+      console.error(e);
     }
+  }
 
-    //Get Current User
+  //Get Current User
 
-    async authGetCurrentUser() {
-        const user = auth.currentUser
-        if (user) {
-            return user
-        } else {
-            console.error('user is null')
-        }
-        return user
+  async authGetCurrentUser() {
+    const user = auth.currentUser;
+    if (user) {
+      return user;
+    } else {
+      console.error("user is null");
     }
+    return user;
+  }
 
-    //SignOut
+  //SignOut
 
-    async authSignOut() {
-        return signOut(auth).then(() => {
-        })
-    }
+  async authSignOut() {
+    return signOut(auth).then(() => {});
+  }
 }
